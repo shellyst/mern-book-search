ddc27191b5150dd309e56e7ffd18e586d0a13088
Michelle <mchristine.stone91@gmail.com>
2022-03-25 23:59:55 -0400

    Made preliminary changes to server.js to integrate Apollo server

--- server/server.js
+++ server/server.js
@@ -1,21 +1,39 @@
-const express = require('express');
-const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const express = require("express");
+const path = require("path");
+// Import Apollo server.
+const { ApolloServer } = require("apollo-server-express");
+// Import Middleware.
+const { authMiddleware } = require("./utils/auth");
+// Import typeDefs and resolvers.
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Apollo server.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+// Start the Apollo server.
+// await server.start();
+
+// Integrate Apollo server with the Express application as middleware.
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 app.use(routes);
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
